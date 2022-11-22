6c57c06123d7b4516b1c0ad63b24599a17b26539
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-22 16:59:35 +0900

    Update index.php

--- var/www/lostpet.jp/public_html/index.php
+++ var/www/lostpet.jp/public_html/index.php
@@ -2,6 +2,19 @@
 
 declare(strict_types=1);
 
+if ("dev.lostpet.jp" === $_SERVER["SERVER_NAME"]) {
+  ini_set("opcache.enable", "0");
+  header("x-robots-tag: noindex");
+  header("cache-control: no-cache,no-store,must-revalidate,must-understand,private");
+} elseif ("localhost" === $_SERVER["SERVER_NAME"]) {
+  require __DIR__ . "/../lib/preload.php";
+}
+
+// TODO: op-cache有効時に解除
+require __DIR__ . "/../lib/preload.php";
+
+require __DIR__ . "/../lib/Config/index.php";
+
 set_error_handler(function ($no, $str, $file, $line) {
   new Discord('error', [
     "content" => "```{$no}: {$str}```\n{$file}:{$line}",
@@ -13,7 +26,7 @@ register_shutdown_function(function () {
 
   if ($errors) {
     new Discord('error', [
-      "content" => "```" . json_encode($errors, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "```",
+      "content" => "```" . json_encode($errors, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "```\n" . _PATH_,
     ]);
   }
 });
@@ -39,11 +52,35 @@ if (php_sapi_name() === "cli") {
   define("_UA_", $_SERVER["HTTP_USER_AGENT"] ?? "human");
 }
 
+if (2 === _STAGE_) {
+  if (_IP_ !== Secret::get("/admin/ip.txt")) {
+    Document::error(500);
+  }
+
+  if (false !== strpos(_PATH_, ".")) {
+    $pathinfo = pathinfo(_PATH_);
+
+    if (in_array($pathinfo["extension"], ["css", "js",], true)) {
+      $path = _DIR_ . "/public_html" . _PATH_;
+      if (!file_exists($path)) Document::error(404);
+      Etag::generate(_PATH_, filemtime($path));
+      $body = gzencode(file_get_contents($path), 4);
+      header("cache-control: max-age=1,immutable");
+      header("content-type:" . ("js" === $pathinfo["extension"] ? "application/javascript" : "text/css") . ";charset=UTF-8");
+      header("content-length:" . strlen($body));
+      header("content-encoding:gzip");
+      Etag::echo();
+      echo $body;
+      exit;
+    }
+  }
+}
+
 if (3 === _REQUEST_ || (2 === _STAGE_ && _PATH_ === "/test")) {
   session_cache_limiter("");
   session_start();
 }
 
-Document::create(1 === _REQUEST_ ? new HTMLProcess : new APIProcess);
+Document::create(1 === _REQUEST_ ? new HTMLDocument : new JSONDocument);
 
 exit;
