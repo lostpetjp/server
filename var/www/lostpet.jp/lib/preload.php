6de9e16d497a6971acfb78ff72fe666ff45b310f
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-24 12:29:06 +0900

    Update preload.php

--- var/www/lostpet.jp/lib/preload.php
+++ var/www/lostpet.jp/lib/preload.php
@@ -5,13 +5,15 @@
  */
 
 foreach ([
-  'Discord',
-  'Document',
-  'Etag',
-  'Json2Node',
-  'RDS',
-  'Secret',
-  'utils',
+  "Discord",
+  "Document",
+  "Etag",
+  "Json2Node",
+  "RDS",
+  "Recaptcha",
+  "S3",
+  "Secret",
+  "utils",
 ] as $name) {
   require __DIR__ . "/{$name}/index.php";
 }
