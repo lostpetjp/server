5675ddaad942333a9b6e6a5bdf61ac49c6cf3a70
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-25 20:32:11 +0900

    Update preload.php

--- var/www/lostpet.jp/lib/preload.php
+++ var/www/lostpet.jp/lib/preload.php
@@ -5,13 +5,20 @@
  */
 
 foreach ([
+  "Batch",
+  "Cases",
   "Discord",
   "Document",
+  "Encode",
   "Etag",
   "Json2Node",
+  "Log",
+  "MediaRelation",
+  "Queue",
   "RDS",
   "Recaptcha",
   "S3",
+  "SES",
   "Secret",
   "utils",
 ] as $name) {
