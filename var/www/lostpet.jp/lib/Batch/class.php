e439d1b12bbc460e727dfc666185adf9254c28fb
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-25 20:34:48 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Batch/class.php
+++ var/www/lostpet.jp/lib/Batch/class.php
@@ -4,33 +4,52 @@ declare(strict_types=1);
 
 class Batch
 {
-  static public function run()
+  static public array $entries = [
+    1 => [  // 案件のクローン
+      "span" => 0,
+      "hours" => [],
+    ],
+    2 => [  // メディアファイルのクローン
+      "span" => 0,
+      "hours" => [],
+    ],
+  ];
+
+  static public function dispatch()
   {
-    $rows = RDS::fetchAll("SELECT * FROM `batch`;");
+    $rows = RDS::fetchAll("SELECT * FROM `batch` WHERE `type`=?;", [
+      1,
+    ]);
 
     array_multisort(array_column($rows, 'updated_at'), SORT_ASC, $rows);
 
-    foreach ($rows as $row) {
-      $id = $row['id'];
-      $updated_at = $row['updated_at'];
-      $span = $row['span'];
-      $hours = $row['hour'];
-      if ($hours) $hours = json_decode($hours, true);
+    foreach (self::$entries as $id => $entry) {
+      foreach ($rows as $row) {
+        if ($row["id"] === $id) {
+          $updated_at = $row['updated_at'];
+          $span = $entry["span"];
+          $hours = $entry["hours"];
 
-      if (
-        $_SERVER['REQUEST_TIME'] > ($updated_at + $span)
-        && (!$hours || in_array((int)date("H"), $hours, true))
-      ) {
-        require __DIR__ . "/entries/{$id}.php";
+          if (
+            $_SERVER['REQUEST_TIME'] > ($updated_at + $span)
+            && (!$hours || in_array((int)date("H"), $hours, true))
+          ) {
+            self::run($id);
+
+            RDS::execute("UPDATE `batch` SET `updated_at`=? WHERE `type`=? AND `id`=? LIMIT 1;", [
+              $_SERVER["REQUEST_TIME"],
+              1,
+              $id,
+            ]);
+          }
+        }
       }
     }
   }
 
-  static public function update(int $id)
+  static public function run(int $id)
   {
-    RDS::execute("UPDATE `batch` SET `updated_at`=? WHERE `id`=? LIMIT 1;", [
-      $_SERVER['REQUEST_TIME'],
-      $id,
-    ]);
+    require_once __DIR__ . "/entries/{$id}.php";
+    "Batch{$id}"::dispatch();
   }
 }
