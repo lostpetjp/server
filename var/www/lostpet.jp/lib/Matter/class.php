0790f65dd3f50b879e84cfd0ded69cd830fb7d3f
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 18:00:22 +0900

    Create class.php

---
+++ var/www/lostpet.jp/lib/Matter/class.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+class Matter
+{
+  static public array $data = [
+    1 => [
+      "id" => 1,
+      "name" => "lost",
+      "search" => 1,
+      "title" => "迷子",
+    ],
+    2 => [
+      "id" => 2,
+      "name" => "find",
+      "search" => 1,
+      "title" => "保護",
+    ],
+    3 => [
+      "id" => 3,
+      "name" => "witness",
+      "search" => 0,
+      "title" => "目撃",
+    ],
+    4 => [
+      "id" => 4,
+      "name" => "rainbow",
+      "search" => 0,
+      "title" => "虹の橋",
+    ],
+  ];
+}
