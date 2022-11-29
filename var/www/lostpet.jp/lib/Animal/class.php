522869c34d5076eda10b819bb94fe32a64b39a65
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 17:59:10 +0900

    Create class.php

---
+++ var/www/lostpet.jp/lib/Animal/class.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+class Animal
+{
+  static public array $data = [
+    1 => [
+      "id" => 1,
+      "name" => "dog",
+      "title" => "犬",
+      "search" => 1,
+    ],
+    2 => [
+      "id" => 2,
+      "name" => "cat",
+      "title" => "猫",
+      "search" => 1,
+    ],
+    3 => [
+      "id" => 3,
+      "name" => "bird",
+      "title" => "鳥",
+      "search" => 1,
+    ],
+    4 => [
+      "id" => 4,
+      "name" => "turtle",
+      "title" => "亀",
+      "search" => 1,
+    ],
+    5 => [
+      "id" => 5,
+      "name" => "rabbit",
+      "title" => "ウサギ",
+      "search" => 0,
+    ],
+    6 => [
+      "id" => 6,
+      "name" => "ferret",
+      "title" => "フェレット",
+      "search" => 0,
+    ],
+    7 => [
+      "id" => 7,
+      "name" => "hamster",
+      "title" => "ハムスター",
+      "search" => 0,
+    ],
+    8 => [
+      "id" => 8,
+      "name" => "flying-squirrel",
+      "title" => "モモンガ",
+      "search" => 0,
+    ],
+    99 => [
+      "id" => 99,
+      "name" => "others",
+      "title" => "その他",
+      "search" => 1,
+    ],
+  ];
+}
