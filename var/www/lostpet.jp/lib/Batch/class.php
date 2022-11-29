af21982483fdbd2e185ebf100bcc7e5ac92360a0
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 17:43:33 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Batch/class.php
+++ var/www/lostpet.jp/lib/Batch/class.php
@@ -4,25 +4,6 @@ declare(strict_types=1);
 
 class Batch
 {
-  static public array $entries = [
-    1 => [  // 案件のクローン
-      "span" => 300,
-      "hours" => [],
-    ],
-    2 => [  // メディアファイルのクローン
-      "span" => 0,
-      "hours" => [],
-    ],
-    3 => [  // コメントのクローン
-      "span" => 0,
-      "hours" => [],
-    ],
-    4 => [  // `media-relation`のclean up
-      "span" => 43200,
-      "hours" => [4, 5,],
-    ],
-  ];
-
   static public function dispatch()
   {
     $rows = RDS::fetchAll("SELECT * FROM `batch` WHERE `type`=?;", [
@@ -31,26 +12,25 @@ class Batch
 
     array_multisort(array_column($rows, 'updated_at'), SORT_ASC, $rows);
 
-    foreach (self::$entries as $id => $entry) {
-      foreach ($rows as $row) {
-        if ($row["id"] === $id) {
-          $updated_at = $row['updated_at'];
-          $span = $entry["span"];
-          $hours = $entry["hours"];
+    foreach ($rows as $row) {
+      $id = $row["id"];
+      $updated_at = $row['updated_at'];
 
-          if (
-            $_SERVER['REQUEST_TIME'] > ($updated_at + $span)
-            && (!$hours || in_array((int)date("H"), $hours, true))
-          ) {
-            self::run($id);
+      require_once __DIR__ . "/entries/{$id}.php";
+      $span = "Batch{$id}"::$span;
+      $hours = "Batch{$id}"::$hours;
 
-            RDS::execute("UPDATE `batch` SET `updated_at`=? WHERE `type`=? AND `id`=? LIMIT 1;", [
-              $_SERVER["REQUEST_TIME"],
-              1,
-              $id,
-            ]);
-          }
-        }
+      if (
+        $_SERVER['REQUEST_TIME'] > ($updated_at + $span)
+        && (!$hours || in_array((int)date("H"), $hours, true))
+      ) {
+        self::run($id);
+
+        RDS::execute("UPDATE `batch` SET `updated_at`=? WHERE `type`=? AND `id`=? LIMIT 1;", [
+          $_SERVER["REQUEST_TIME"],
+          1,
+          $id,
+        ]);
       }
     }
   }
