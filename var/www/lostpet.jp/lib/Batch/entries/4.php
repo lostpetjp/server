f99b2ecdd7dba53eae71edd5db1b45120ec47176
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 17:43:44 +0900

    Update 4.php

--- var/www/lostpet.jp/lib/Batch/entries/4.php
+++ var/www/lostpet.jp/lib/Batch/entries/4.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 class Batch4
 {
+  static public int $span = 86400;
+  static public array $hours = [2, 3, 4, 5,];
+
   static public function dispatch(): void
   {
     RDS::execute("DELETE FROM `media-relation` WHERE `status`=?;", [
@@ -11,5 +14,9 @@ class Batch4
     ]);
 
     RDS::execute("OPTIMIZE TABLE `media-relation`;");
+
+    new Discord("batch", [
+      "content" => "`batch4`を実行しました。",
+    ]);
   }
 }
