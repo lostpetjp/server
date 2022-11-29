117149c9ac052f6b3c9286553661d08a28b54a17
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 17:43:38 +0900

    Update 1.php

--- var/www/lostpet.jp/lib/Batch/entries/1.php
+++ var/www/lostpet.jp/lib/Batch/entries/1.php
@@ -5,10 +5,18 @@ declare(strict_types=1);
 
 class Batch1
 {
+  static public int $span = 600;
+  static public array $hours = [];
+
   static public function dispatch(): void
   {
+
     require_once _DIR_ . "/lib/migrate/index.php";
 
     MigrateCase::batch();
+
+    new Discord("batch", [
+      "content" => "`batch1`を実行しました。",
+    ]);
   }
 }
