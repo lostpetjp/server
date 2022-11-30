ff92edc38c8017a8c01e4e69fdb4ece23e39c152
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-30 21:14:13 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Media/class.php
+++ var/www/lostpet.jp/lib/Media/class.php
@@ -13,4 +13,27 @@ class Media
 
     return "upload/src/media/" . max(1, ($start1)) . "-" . ($start1 + 100000000 - 1) . "/" . max(1, ($start2)) . "-" . ($start2 + 1000000 - 1) . "/" . max(($start3), 1) . "-" . ($start3 + 10000 - 1) . "/" . max(($start4), 1) . "-" . ($start4 + 100 - 1) . "/" . $name;
   }
+
+  // m138417s1599x1199z.jpg
+
+  static public function parse(string $name): ?array
+  {
+    if (preg_match("/\Am([0-9]+)s([0-9]+)x([0-9]+)z\.(jpg|png)\z/", $name, $matches)) {
+      $id = (int)$matches[1];
+      $height = (int)$matches[3];
+      $width = (int)$matches[2];
+      $extension = $matches[4];
+
+      return [
+        "id" => $id,
+        "height" => $height,
+        "width" => $width,
+        "extension" => $extension,
+        "prefix" => "m{$id}s{$width}x{$height}z",
+        "suffix" => ".{$extension}",
+      ];
+    }
+
+    return null;
+  }
 }
