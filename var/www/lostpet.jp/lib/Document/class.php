7c94ef5794365a2d9fc4fe2c274175c77ca03f4e
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-30 10:19:07 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Document/class.php
+++ var/www/lostpet.jp/lib/Document/class.php
@@ -24,4 +24,16 @@ class Document
 
     exit;
   }
+
+  static public function redirect(string $pathname, int $cache_time = 600): void
+  {
+    if (1 === _REQUEST_) {
+      if (_PATH_ !== $pathname) {
+        http_response_code(301);
+        header("cache-control: max-age={$cache_time},public,immutable");
+        header("location: https://" . _SERVER_ . $pathname);
+        exit;
+      }
+    }
+  }
 }
