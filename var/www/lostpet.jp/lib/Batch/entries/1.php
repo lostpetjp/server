2a9ba803bc30bf1a8f85dc0d11573ce315c1e62d
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-01 22:38:12 +0900

    Update 1.php

--- var/www/lostpet.jp/lib/Batch/entries/1.php
+++ var/www/lostpet.jp/lib/Batch/entries/1.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 
 class Batch1
 {
-  static public int $span = 600;
+  static public int $span = 30;
   static public array $hours = [];
 
   static public function dispatch(): void
