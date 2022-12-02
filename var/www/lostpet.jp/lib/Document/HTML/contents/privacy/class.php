6471534936441edf3adf2b5414cf2d7d927836ea
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-02 17:24:29 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Document/HTML/contents/privacy/class.php
+++ var/www/lostpet.jp/lib/Document/HTML/contents/privacy/class.php
@@ -35,7 +35,7 @@ class HTMLDocumentPrivacyContent implements HTMLDocumentContentInterface
   static public function create(string $pathname): array
   {
     self::$cache_time = 1 === _STAGE_ ? 600 : 1;
-    Etag::generate(_PATH_,  max(filemtime(__FILE__), (int)strtotime(date("Y-m-d H:" . (1 === _STAGE_ ? "i" : "00") . ":00"))));
+    Etag::generate(_PATH_,  max(filemtime(__FILE__)));
 
     return [
       "title" => "プライバシーポリシー",
