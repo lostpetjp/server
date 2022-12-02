110b4644a85538dcfad04f2b6f374a6330b2c1d3
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-02 10:19:55 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Etag/class.php
+++ var/www/lostpet.jp/lib/Etag/class.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+// cloudfrontは、nameが同一だと200を返しても、304を返してしまう？ (要確認)
+// timeをnameに含めているので、実質的にtimeを比較する意味はない
 class Etag
 {
   static public ?string $time = null;
@@ -9,13 +11,13 @@ class Etag
 
   static public function generate(string $name, int $time): void
   {
-    $i_time = $_SERVER["HTTP_IF_MODIFIED_SINCE"] ?? null;
+    // $i_time = $_SERVER["HTTP_IF_MODIFIED_SINCE"] ?? null;
     $i_name = $_SERVER["HTTP_IF_NONE_MATCH"] ?? null;
 
     self::$time = gmdate('D, d M Y H:i:s T', $time);
     self::$name = md5($name . self::$time);
 
-    if ($i_time && $i_time >= self::$time && $i_name && false !== strpos($i_name, '"' . self::$time . '"')) {
+    if ($i_name && false !== strpos($i_name, '"' . self::$time . '"')) { // $i_time && strtotime($i_time) >= strtotime(self::$time) && 
       http_response_code(304);
       header('cache-control: max-age=1,public');
       self::echo();
