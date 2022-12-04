e751b6517c758c1b719d9160a25c0a7c8ec0f6b1
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-04 20:55:09 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Document/JSON/contents/data/class.php
+++ var/www/lostpet.jp/lib/Document/JSON/contents/data/class.php
@@ -9,7 +9,7 @@ class JSONDocumentData
   static public function create()
   {
     self::$cache_time = 1 === _STAGE_ ? 3600 : 600;
-    Etag::generate(_PATH_,  max(filemtime(__FILE__), (int)strtotime(date("Y-m-d " . (1 === _STAGE_ ? "00:00" : "H:00") . ":00"))));
+    Etag::generate(_PATH_,  filemtime(__FILE__));
 
     return [
       "matter" => Matter::$data,
