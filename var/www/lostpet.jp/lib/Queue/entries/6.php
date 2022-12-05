de90ae5456e780598c239faf9521d25940e5aeed
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-05 20:41:39 +0900

    Create 6.php

---
+++ var/www/lostpet.jp/lib/Queue/entries/6.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * 掲載期限による終了を処理する
+ */
+class Queue6
+{
+  static public int $type = 6;
+
+  static public function dispatch(int $id)
+  {
+    $case_data = RDS::fetch("SELECT `id`, `expires_at` FROM `case` WHERE `id`=? AND `archive`=? AND `status`=? AND `publish`=? AND `expires_at`>? LIMIT 1;", [
+      $id,
+      0,
+      1,
+      1,
+      0,
+    ]);
+
+    if ($case_data) {
+      if (!($_SERVER["REQUEST_TIME"] > $case_data["expires_at"])) {
+        return Queue::update(self::$type, $id, 3600);
+      }
+
+      RDS::execute("UPDATE `case` SET `publish`=?, `ends_at`=?, `updated_at`=? WHERE `id`=? LIMIT 1;", [
+        0,
+        $_SERVER["REQUEST_TIME"],
+        $_SERVER["REQUEST_TIME"],
+        $id,
+      ]);
+
+      CaseCount::updateAll(true);
+
+      new Discord("case", [
+        "content" => "掲載期限により終了しました。 <https://lostpet.jp/{$id}>",
+      ]);
+    }
+
+    Queue::delete(self::$type, $id);
+  }
+}
