cc7b569dc4ec29935ef4273dc25f2ccfaa9ac9f9
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-05 20:35:38 +0900

    Update 2.php

--- var/www/lostpet.jp/lib/Queue/entries/2.php
+++ var/www/lostpet.jp/lib/Queue/entries/2.php
@@ -6,17 +6,9 @@ declare(strict_types=1);
  */
 class Queue2
 {
-  static private int $id = 2;
+  static private int $type = 2;
 
   static public function dispatch(int $id): void
   {
-    $case_data = RDS::fetch("SELECT * FROM `case` WHERE `id`=? LIMIT 1;", [
-      $id,
-    ]);
-
-    // 関連のログを全て削除する
-    // 削除し切れない場合は、次回に回す
-
-    // Queue::delete(self::$id, $id);
   }
 }
