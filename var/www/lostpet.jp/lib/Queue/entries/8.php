05148faf91f821e941d0eb351e47ade3290e924a
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-07 00:21:32 +0900

    Update 8.php

--- var/www/lostpet.jp/lib/Queue/entries/8.php
+++ var/www/lostpet.jp/lib/Queue/entries/8.php
@@ -31,13 +31,32 @@ class Queue8
         ]);
 
         if ($case_data) {
+          $case_body = json_decode($case_data["body"], true);
+
           // Discordへの通知
           new Discord("comment", [
-            "content" => " :speech_balloon: コメントが投稿されました。 <https://lostpet.jp/" . $comment_data["case"] . ">"
+            "content" => ":speech_balloon: コメントが投稿されました。 <https://lostpet.jp/" . $comment_data["case"] . ">"
               . "\n"
               . (!$comment_data["parent"] ? "\n> **" . $comment_data["head"]["title"] . "**" : "")
               . "\n> " . implode("\n> ", explode("\n", mb_substr($comment_data["body"]["description"], 0, 1500)))
               . "\n　​　",
+          ]);
+
+          // コミュニティに新着通知
+          new Discord("notify-comment", [
+            "content" => ":speech_balloon: コメントが投稿されました。 <https://lostpet.jp/" . $comment_data["case"] . ">"
+              . (!$comment_data["private"] ? "\n"
+                . (!$comment_data["parent"] ? "\n> **" . $comment_data["head"]["title"] . "**" : "")
+                . "\n> " . implode("\n> ", explode("\n", mb_substr($comment_data["body"]["description"], 0, 1500)))
+                . "\n　​　" : ""),
+          ]);
+
+          new Slack("notify-comment", [
+            "text" => ":speech_balloon: コメントが投稿されました。 <https://lostpet.jp/" . $comment_data["case"] . ">"
+              . (!$comment_data["private"] ? "\n"
+                . (!$comment_data["parent"] ? "\n> **" . $comment_data["head"]["title"] . "**" : "")
+                . "\n> " . implode("\n> ", explode("\n", mb_substr($comment_data["body"]["description"], 0, 1500)))
+                . "\n　​　" : ""),
           ]);
 
           // メール通知
@@ -48,6 +67,7 @@ class Queue8
           if ($email_encode) {
             $email_decode = Encode::decode("/email/salt.txt", $email_encode);
 
+            // 管理に新着通知
             new Discord("queue", [
               "content" => "[シミュレート] 新着コメントをオーナーにメールアドレスで通知しました。 <https://lostpet.jp/{$case_id}>",
             ]);
@@ -57,7 +77,8 @@ class Queue8
             $index = 0;
 
             $title = "[迷子ペットのデータベース] 新しいコメントがありました";
-            $body = "新着のコメントがありました。下記は全部は表示されてないので、ページでコメント全文を確認して下さい。"
+            $body = "新着のコメントがありました。"
+              . "\n全部は表示されてないので、ページでコメント全文を確認して下さい。"
               . "\n"
               . "\n--------------------------------------------------"
               . ($comment_title ? "\n[{$comment_title}]" : null)
