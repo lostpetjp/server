950d4719aa6bfb7b5ad83698ff01273f3484b4f4
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-10 14:24:03 +0900

    Update class.php

--- var/www/lostpet.jp/lib/Comment/class.php
+++ var/www/lostpet.jp/lib/Comment/class.php
@@ -4,4 +4,53 @@ declare(strict_types=1);
 
 class Comment
 {
+  static public function getMediaIds(array $items): array
+  {
+    $items = array_map(fn (array $row) => [
+      "body" => is_string($row["body"] ?? null) ? json_decode($row["body"], true) : null,
+      // "head" => is_string($row["head"] ?? null) ? json_decode($row["head"], true) : null,
+    ] + $row, $items);
+
+    $media_ids = [];
+
+    for ($i = 0; count($items) > $i; $i++) {
+      foreach ($items[$i]["body"]["photos"] ?? [] as $photo) $media_ids[] = $photo;
+      foreach ($items[$i]["body"]["videos"] ?? [] as $video) $media_ids[] = $video;
+    }
+
+    return [...array_unique($media_ids)];
+  }
+
+  static public function parse(array $items, ?array $media_map = null)
+  {
+    $items = array_map(fn (array $row) => [
+      "body" => is_string($row["body"] ?? null) ? json_decode($row["body"], true) : null,
+      "head" => is_string($row["head"] ?? null) ? json_decode($row["head"], true) : null,
+    ] + $row, $items);
+
+    if (null === $media_map) {
+      $media_ids = self::getMediaIds($items);
+
+      $rows = $media_ids ? RDS::fetchAll("SELECT `id`, `name` FROM `media` WHERE `id` IN (" . implode(",", array_fill(0, ($limit = count($media_ids)), "?")) . ") AND `status`=? LIMIT {$limit};", [
+        ...$media_ids,
+        1,
+      ]) : [];
+
+      $media_map = array_combine(array_column($rows, "id"), array_column($rows, "name"));
+    }
+
+    for ($i = 0; count($items) > $i; $i++) {
+      if (is_array($items[$i]["body"]["photos"] ?? null)) {
+        $items[$i]["body"]["photos"] = array_filter(array_map(fn (int $entry) => $media_map[$entry] ?? null, $items[$i]["body"]["photos"]), fn (string | null $entry) => is_string($entry));
+      }
+
+      if (is_array($items[$i]["body"]["videos"] ?? null)) {
+        $items[$i]["body"]["videos"] = array_filter(array_map(fn (int $entry) => $media_map[$entry] ?? null, $items[$i]["body"]["videos"]), fn (string | null $entry) => is_string($entry));
+      }
+
+      if (array_key_exists("body", $items[$i]) && null === $items[$i]["body"]) unset($items[$i]["body"]);
+    }
+
+    return $items;
+  }
 }
