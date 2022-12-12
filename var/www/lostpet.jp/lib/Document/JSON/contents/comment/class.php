1c52755581f6861df8801bed84776eb62ac161c1
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-12 22:17:16 +0900

    Create class.php

---
+++ var/www/lostpet.jp/lib/Document/JSON/contents/comment/class.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+class JSONDocumentComment
+{
+  static public int $cache_time = 1;
+
+  static public function create()
+  {
+    $case_id = (int)($_GET["id"] ?? null);
+
+    if ($case_id) {
+      $case_data = RDS::fetch("SELECT * FROM `case` WHERE `id`=? LIMIT 1;", [
+        $case_id,
+      ]);
+
+      $version = $case_data["updated_at"];
+      Etag::generate(_PATH_,  max(filemtime(__FILE__), $version));
+
+      $case_data["head"] = json_decode($case_data["head"], true);
+      $case_data["body"] = json_decode($case_data["body"], true);
+
+      // for media
+      [$case_data,] = Cases::parse([$case_data,]);
+
+      var_dump($case_data);
+      exit;
+
+      return [
+        "data" => $case_data,
+        "status" => true,
+      ];
+    }
+
+    return [
+      "status" => false,
+    ];
+  }
+}
